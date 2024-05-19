1216f382b8b65fd3e3b30f2ed7239c547ba75f91
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-19 15:07:52 +0530

    intial commit

---
+++ assignment_two_list_view/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'registration_page.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Iphone Shopie',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => RegistrationPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
